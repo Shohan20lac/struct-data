34fb1307ecd7c4d2d16e755c24181520ff49ab6e
Shohan Rahman <r.shohan12@gmail.com>
2021-08-11 16:14:17 +0600

    hw3 updated

--- DynamicMemory/HW3(linkedLists)/HW3(linkedLists).cpp
+++ DynamicMemory/HW3(linkedLists)/HW3(linkedLists).cpp
@@ -1,24 +1,149 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+
 using namespace std;
 
-struct page {
-	int pageNum;
-	string content;
-	page* nextpage;
+
+struct Rectangle {
+	int L = 0;
+	int W = 0;
+
+	int area = 0;
+
+	Rectangle( int l, int w) {
+		L = l;
+		W = w;
+		area = L * W;
+	}
 };
+
+void setLW( Rectangle r [] , int size ) {	//randomly initiates a rectangle.
+	for (int i = 0; i < size; i++)
+	{
+		r[i].L = rand() % 100;
+		r[i].W = rand() % 100;
+		cout << "Triangle # " << i << " random L and W set." << endl;
+	}
+}
+
+void setArea(Rectangle r[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		r[i].area = r[i].L * r[i].W;
+		cout << "Triangle # " << i << " area set." << endl;
+	}
+}
+
+void showTriangles(Rectangle* r[], int size){
+	for (int i = 0; i < size; i++)
+	{
+		cout << "Triangle # " << i << endl;
+		cout << " area = " << r[i]->area << endl;
+
+	}
+}
+
+void makeJuice() {
+	string juice = "";
+	cout << "juice name: ";
+
+	getline(cin, juice);
+	cout << juice;
+}
+
+string getString () {
+	string str = "";
+	getline(cin, str);
+	return str;
+}
+
+int getInteger() {
+	int i = 0;
+
+	stringstream ss;
+	string str = getString();
+	ss << str;
+	
+	ss >> i;
+	return i;
+}
+
+struct node {
+	string name;
+	int age;
+	node* next;
+	node* previous;
+
+	
+	node(string n, int a) {
+		name = n;
+		age = a;
+		next = nullptr;
+		previous = nullptr;
+		
+	}
+};
+
+struct page {
+	int pagenum;
+	page* nextpage = nullptr;
+
+	page(int pn) {
+		pagenum = pn;
+	}
+};
+
+
+
+node* initNode(string name, int age) {
+	node* head = new node(name, age);
+	return head;
+}
+
+void traverseAll(node* thisnode) {
+	int count = 0;
+	while (thisnode->next != nullptr) {
+		thisnode = thisnode->next;
+		cout << "\nTraversed a node " << count++ << " times." << endl;
+	}
+		
+
+}
+
+void addNode( node* n, string name, int age) {
+	node* newnode = new node(name, age);
+	n->next = newnode;
+
+}
+
+
 
 
 int main()
 {
 	int choice = 1000;
 
+	//MAIN MENU 
 	while (choice != 0) 
 	{
+		//FRONTEND
 		cout << "\nEnter choice: ";
 		cin >> choice;
+		cin.ignore();
 
+		//BACKEND
 		switch (choice) {
+
+			case 40:
+			{
+				string juicename = "";
+				juicename = getString();
+				cout << juicename;
+			} break;
+
+
 			case 1://helloworld
 			{
 				cout << "hello world!";
@@ -26,39 +151,84 @@ int main()
 
 			case 2: //dynamic 2D array
 			{
-				int coursecount = 0; cin >> coursecount;
-				int* s1 = new int[3]{ 1,2,3 };
+				const int nStudents = 3;
+				int* students[nStudents];
 
-				for (int i = 0; i < coursecount; i++)
+				for (int k = 0; k < nStudents; ++k)
 				{
-					s1[i] = rand()
-				}
-				
-				int* b = new int[2]{ 4,6 };
-				int* c = new int[4]{ 7,8,9, 10 };
-				int x[3] = { 3,2,4 };
+					int courseTaken = 0;
+					
+					cout << "enter number of courses taken:" << endl;
+					cin >> courseTaken;
+					int* nCourses = new int[courseTaken];
 
-				int* students[3];
-				students[0] = a;
-				students[1] = b;
-				students[2] = c;
-
-
-				int* row = a;
-
-				for (int k = 0; k < 3; k++)
-				{
-					for (int i = 0; i < x[k]; i++)
+					for (int i = 0; i < courseTaken; ++i)
 					{
-						cout << *(students[k]++) << " ";
+						nCourses[i] = rand() % 100;
 					}
+					students[k] = nCourses;
 				}
+
 			}
 
-			case 3 :
+			case 3 :	//setting up and displaying the dimensions of a rectangle
 			{
+				const int rectcount = 4;
+				cout << "\n Number of rectangles? " << endl;
 
-			}
+				
+				Rectangle* rects[rectcount];
+
+				for (int i = 0; i < rectcount; i++)
+				{
+					rects[i] = new Rectangle(rand() % 100, rand() % 100);
+					cout << "\n New random rectangle created " << i << endl;
+				}
+
+				showTriangles(rects, rectcount);
+
+
+
+				cout << "\end" << endl;
+
+			}break; 
+
+			case 4:	//linked list START
+			{
+				page* ptr;
+				int pagecount = 1;
+
+				//first node
+				page* p1 = new page(pagecount); pagecount++;
+				page* head = p1;
+				ptr = head;
+				cout << "Page pumber " << ptr->pagenum << " created." << endl;
+
+				//second node
+				page* p2 = new page(pagecount); pagecount++;
+				ptr = head;
+				ptr->nextpage = p2;
+				ptr = p2;
+				cout << "Page pumber " << ptr->pagenum << " created." << endl;
+
+				//third node
+				page* p3 = new page(pagecount); pagecount++;
+				ptr = head;
+				ptr = ptr->nextpage;	//traverse once to get to second node
+				ptr->nextpage = p3;		//from second node, assign p3 as the third node and link it.
+				ptr = p3;
+				cout << "Page pumber " << ptr->pagenum << " created." << endl;
+				
+
+
+
+
+
+
+				
+				
+
+			}break;
 
 
 
