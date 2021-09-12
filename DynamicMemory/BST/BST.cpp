8da60a389edfa1fc343bd8a802d6cf745679e8ba
Shohan Rahman <r.shohan12@gmail.com>
2021-09-12 19:15:12 +0600

    countleaves() implemented
    implemented three  different ways to count the number of leaves on a tree.

--- DynamicMemory/BST/BST.cpp
+++ DynamicMemory/BST/BST.cpp
@@ -11,6 +11,13 @@ struct node {
     }
 };
 
+struct dllnode {
+    int data = 0;
+    dllnode* next = nullptr;
+    dllnode* prev = nullptr;
+    
+};
+
 void printNumber(int x) {
     //peculiar recursion example. Observe wisely!
     if (x > 10) return;
@@ -143,30 +150,142 @@ void postorder ( node* a ) {
     cout << a->data << " ";
 }
 
+void showNthLevel( node*a, int n, int i) {
+
+    if (a == nullptr) {     //trivial base case reached
+        cout << "Leaf node exceeded past level" << i << ". Returning..." << endl;
+        return;
+    }
+
+    if ( i==n ) {           //intended base case reached
+        cout << "Target level " << i << " reached. Printing: " << (a->data) << endl;
+        return;
+    }
+
+    //else, keep recursin'.
+    showNthLevel(a->left, n, i+1);
+    showNthLevel(a->right, n, i+1);
+}
+
+int countleavesintref( node*a, int &count) {
+
+    if (a == nullptr) {                                     //trivial base case
+        cout << "nullpointer reached. Returning..." << endl << endl;
+        return 0;
+    }
+
+    cout << "Currently at node " << a << " with value " << a->data << endl;
+
+    if ( a->left==nullptr  &&  a->right == nullptr ) {      //target base case 
+        count++;
+        cout << "Found leaf with value " << a->data << ". Leafcount increased to " << count << endl;
+        return count;
+    }
+
+    countleavesintref(a->left, count);
+    countleavesintref(a->right, count);
+
+    return count;
+
+    /*
+    return (
+        countleavesintref(a->left, count)
+        +
+        countleavesintref(a->right, count)
+    );            
+    */
+}
+
+int countleavesintval( node*a, int count) {
+
+    if (a == nullptr) {                                     //trivial base case
+        cout << "nullpointer reached. Returning..." << endl << endl;
+        return 0;
+    }
+
+    cout << "Currently at node " << a << " with value " << a->data << endl;
+
+    if ( a->left==nullptr  &&  a->right == nullptr ) {      //target base case 
+        count++;
+        cout << "Found leaf with value " << a->data << ". Leafcount increased to " << count << endl;
+        return count;
+    }
+
+    return (
+        countleavesintval(a->left, count)
+        +
+        countleavesintval(a->right, count)
+    );  
+}
+
+void countleavesvoid(node* a, int& count) {
+    
+    if (a == nullptr) {                                     //trivial base case
+        cout << "nullpointer reached. Returning..." << endl;
+        return;
+    }
+
+    if ( a->left == nullptr  &&  a->right == nullptr ) {    //intended base case
+        count++;
+        cout << "Leaf reached. Increased leafcount to " << count << endl;
+
+        return;
+    }
+
+    
+    countleavesvoid(a->left, count);
+        
+    countleavesvoid(a->right, count);
+    
+
+    
+    
+}
+
+int countSiblingPairs( node* a, int level, int paircount, int i ) {
+    return 0;
+}
+
+
+bool levelHasSiblingPairs () {    //returns true if the nth level has at least k sibling pairs.
+    return 0;
+}
+
 int main()
 {
     node* root1 = nullptr;
     insert(root1, 10);
     insert(root1, 11);
     insert(root1, 5);
-    insert(root1, 16);
-    insert(root1, 12;
+    //insert(root1, 16);
+    insert(root1, 12);
     insert(root1, 8);
+
+    
+    /*
+    //SHOWING ALL NODES AT LEVEL n; starting iteration at i=1 
+    int n = 2;
+    int i = 1;
+    showNthLevel(root1, n, i);
+    */
 
     //COUNTING LEVELS
     //int answer = countLevelsInt ( root1, 0 );
     //cout << "Answer: " << answer << endl;
 
+    /*
     //SHOWING ALL LEVELS
     cout << "Showing preorder: "; preorder(root1); cout << endl;
     cout << "Showing inorder: "; inorder(root1); cout << endl;
     cout << "Showing postorder: "; postorder(root1); cout << endl;
-    
-    
-    
-    
-    
-    inorder(root1);
-    postorder( root1 );
+    */
 
+    /*
+    int count = 0;
+    count = countleaves(root1, count);
+    cout << count;
+    */
+
+    int count = 0;
+    cout << countleavesint(root1, count);
 }
